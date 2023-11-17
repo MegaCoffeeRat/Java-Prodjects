4af1df4de6c197277c1cfe41cb4d0460e324383f
MegaCoffeeRat <fedorenko.css@gmail.com>
2023-11-17 09:22:49 -0500

    Created Click proj and made background

---
+++ Click/src/core/Engine.java
@@ -0,0 +1,67 @@
+package core;
+
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.BasicGameState;
+import org.newdawn.slick.state.StateBasedGame;
+
+public class Engine extends BasicGameState
+{	
+	private int id;
+	Image bg;
+
+	public Engine(int id)
+	{
+		this.id = id;
+	}
+	
+	public int getID() 
+	{
+		return id;		
+	}
+	
+	public void init(GameContainer gc, StateBasedGame sbg) throws SlickException 
+	{
+		// This code happens when you enter a game state for the *first time.*
+		gc.setShowFPS(true);
+		bg = new Image("res/bg.png");
+	}
+
+	public void update(GameContainer gc, StateBasedGame sbg, int delta) throws SlickException
+	{	
+		// This is updates your game's logic every frame.  NO DRAWING.
+	}
+
+	public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException 
+	{
+		// This code renders shapes and images every frame.
+		g.drawImage(bg, 0,0);
+
+	}
+	
+	public void enter(GameContainer gc, StateBasedGame sbg) throws SlickException 
+	{
+		// This code happens when you enter a gameState.  
+	}
+
+	public void leave(GameContainer gc, StateBasedGame sbg) 
+	{
+		// This code happens when you leave a gameState. 
+	}
+
+	public void keyPressed(int key, char c)
+	{
+		// This code happens every time the user presses a key
+	}
+	
+	public void mousePressed(int button, int x, int y)
+	{
+		// This code happens every time the user presses the mouse
+	}
+	
+	
+
+
+}
